49433905afd4563a7d24a227d935b5ea39a68a42
Rafi Izzaturohman <53922794+rafiizzaturohman@users.noreply.github.com>
2023-09-24 08:57:13 +0700

    On Progress

---
+++ app/components/Card/ServiceCard.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import Link from "next/link";
+import React, { useCallback, useState } from "react";
+import { BiRightArrowAlt, BiWorld } from "react-icons/bi";
+import { FaBed, FaCartPlus, FaPeopleLine } from "react-icons/fa6";
+
+interface ServiceCardProps {
+  title: string;
+  descriptions: string;
+  no: number;
+}
+
+const icons = [{}];
+
+const ServiceCard: React.FC<ServiceCardProps> = ({
+  title,
+  descriptions,
+  no,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
+  return (
+    <div className="flex flex-col w-2/3 md:flex-row md:h-[8vw] md:w-full">
+      {isOpen == false ? (
+        <div
+          className={
+            no == 0
+              ? "bg-[#1F4590] items-center py-3 text-white rounded-tr-lg md:rounded-r-none md:rounded-tl-lg"
+              : no == 3
+              ? "bg-[#1F4590] items-center py-3 text-white rounded-br-lg md:rounded-br-none md:rounded-tr-lg"
+              : "bg-[#1F4590] items-center py-3 text-white rounded-none"
+          }
+          onClick={toggleOpen}
+        >
+          <div className="flex flex-row md:flex-col justify-start items-center px-2 py-1 space-x-3">
+            {no == 0 ? (
+              <BiWorld className="text-xl" color="white" />
+            ) : no == 1 ? (
+              <FaPeopleLine className="text-xl" color="white" />
+            ) : no == 2 ? (
+              <FaCartPlus className="text-xl" color="white" />
+            ) : (
+              <FaBed className="text-xl" color="white" />
+            )}
+
+            <h4 className="font-bold text-xs">{title}</h4>
+          </div>
+        </div>
+      ) : (
+        <div
+          className="bg-[#FFA451] w-[90vw] md:w-auto md:h-[20vw] text-white rounded-r-lg md:rounded-br-none md:rounded-t-lg px-1 py-2"
+          onClick={toggleOpen}
+        >
+          <div className="flex flex-row md:flex-col justify-evenly items-center">
+            <div className="flex flex-col justify-center w-full space-y-2 text-center">
+              <div className="flex flex-auto justify-center">
+                {no == 0 ? (
+                  <BiWorld className="text-2xl" color="white" />
+                ) : no == 1 ? (
+                  <FaPeopleLine className="text-2xl" color="white" />
+                ) : no == 2 ? (
+                  <FaCartPlus className="text-2xl" color="white" />
+                ) : (
+                  <FaBed className="text-2xl" color="white" />
+                )}
+              </div>
+
+              <h4 className="font-bold text-sm">{title}</h4>
+            </div>
+
+            <div className="space-y-2">
+              <p className="text-xs font-medium">{descriptions}</p>
+
+              <Link
+                href={"#"}
+                className="text-[#1B1C1E] text-xs font-semibold hover:underline hover:underline-offset-4 flex items-center"
+              >
+                <p>Learn more</p>
+                <BiRightArrowAlt className="text-base" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ServiceCard;
